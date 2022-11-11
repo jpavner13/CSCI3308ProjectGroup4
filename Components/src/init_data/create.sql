4e0156720c2654f522f99a98c0e94d4dd44b9725
nick0connor <nioc4468@colorado.edu>
2022-11-10 20:51:10 -0700

    Added basics for db

--- Components/src/init_data/create.sql
+++ Components/src/init_data/create.sql
@@ -1,18 +1,45 @@
-DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users(
-    username VARCHAR(50) PRIMARY KEY,
-    password CHAR(60) NOT NULL,
-    location VARCHAR(50) NOT NULL
+DROP TABLE IF EXISTS locations CASCADE;
+CREATE TABLE locations(
+  location_id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(20),
+  country VARCHAR(20)
 );
 
--- INSERT INTO users (username, password, location) VALUES (jpvner13, Pierce, Denver);
--- INSERT INTO users (username, password, location) VALUES (kiethbliss, Kieth, Boulder);
--- INSERT INTO users (username, password, location) VALUES (JunoPark-01, Juno, Colorado Springs);
--- INSERT INTO users (username, password, location) VALUES (nick0cinnor, Nick, Fort Collins);
--- INSERT INTO users (username, password, location) VALUES (jithinrenji, Jithin, Breckenridge);
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users(
+  user_id SERIAL PRIMARY KEY NOT NULL,
+  username VARCHAR(50),
+  password CHAR(60) NOT NULL,
+  location VARCHAR(50) NOT NULL,
+  firstname VARCHAR(20),
+  lastname VARCHAR(20),
+  email VARCHAR(30),
+  location_id INTEGER NOT NULL REFERENCES locations (location_id)
+);
 
--- DROP TABLE IF EXISTS profilePics CASCADE;
--- CREATE TABLE profilePics(
---     username VARCHAR(50) PRIMARY KEY,
---     profilePic BLOB NOT NULL
--- )
+DROP TABLE IF EXISTS posts CASCADE;
+CREATE TABLE posts(
+  post_id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER NOT NULL REFERENCES users (user_id),
+  location_id INTEGER NOT NULL REFERENCES locations (location_id),
+  title VARCHAR(100) NOT NULL,
+  description VARCHAR(300), 
+  image_url VARCHAR(300),
+  rating INTEGER,
+);
+
+DROP TABLE IF EXISTS posts_to_locations CASCADE;
+CREATE TABLE posts_to_locations(
+  location_id INT NOT NULL,
+  post_id INT NOT NULL,
+  FOREIGN KEY (location_id) REFERENCES locations (location_id),
+  FOREIGN KEY (post_id) REFERENCES posts (post_id)
+);
+
+DROP TABLE IF EXISTS users_to_locations CASCADE;
+CREATE TABLE users_to_locations(
+  location_id INT NOT NULL,
+  user_id INT NOT NULL,
+  FOREIGN KEY (location_id) REFERENCES locations (location_id),
+  FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
